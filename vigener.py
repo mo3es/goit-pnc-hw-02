361fbe23844aa56b53e6897c3a806fd5c4719a51
mo3es <baylomv@gmail.com>
2025-10-20 14:51:58 +0300

    Vigener_1 - added version without spaces etc

--- vigener.py
+++ vigener.py
@@ -22,6 +22,25 @@ def vigenere_encrypt(text: str, key: str) -> str:
     return "".join(result)
 
 
+def vigenere_encrypt_short(text: str, key: str) -> str:
+    result = []
+    key_chars = [c.upper() for c in key if c.isalpha()]
+    key_len = len(key_chars)
+
+    key_index = 0
+
+    for char in text:
+        if char.isalpha():
+            base = ord("A") if char.isupper() else ord("a")
+            text_num = ord(char) - base
+            key_num = ord(key_chars[key_index % key_len]) - ord("A")
+            new_num = (text_num + key_num) % 26
+            result.append(chr(base + new_num))
+            key_index += 1
+
+    return "".join(result)
+
+
 def vigenere_decrypt(cipher_text: str, key: str) -> str:
     result = []
     key_chars = [c.upper() for c in key if c.isalpha()]
@@ -60,6 +79,14 @@ def main():
         f"Зашифрований текст: {encrypted_text}..., збережено до файлу {encrypted_filename}"
     )
 
+    encrypted_text_short = vigenere_encrypt_short(text, KEY)
+    encrypted_filename_short = "encrypted_short.txt"
+    with open(encrypted_filename_short, "w", encoding="UTF-8") as f:
+        f.write(encrypted_text_short)
+    print(
+        f"Зашифрований текст: {encrypted_text_short}..., збережено до файлу {encrypted_filename_short}"
+    )
+
     with open(encrypted_filename, "r", encoding="UTF-8") as f:
         text_to_decrypt = f.read()
 
@@ -73,6 +100,17 @@ def main():
     is_successful = text.strip() == decrypted_text.strip()
     print(
         f"Розшифрований текст ідентичний оригіналу: {'Так' if is_successful else 'Ні'}"
+    )
+
+    with open(encrypted_filename_short, "r", encoding="UTF-8") as f:
+        text_to_decrypt = f.read()
+
+    decrypted_text_short = vigenere_decrypt(text_to_decrypt, KEY)
+    decrypted_filename_short = "decrypted_short.txt"
+    with open(decrypted_filename_short, "w", encoding="UTF-8") as f:
+        f.write(decrypted_text_short)
+    print(
+        f"Розшифрований текст: {decrypted_text_short}..., збережено до файлу {decrypted_filename_short}"
     )
 
 
